641eb07f56a7c258bb00ad95a9ed66daf972dc02
MOHAMMAD-TARIQUE-95 <mhdtrq80@gmail.com>
2024-09-17 15:10:00 +0530

    Corrected the user repository

--- src/main/java/com/zoom/repository/UserRepository.java
+++ src/main/java/com/zoom/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.zoom.repository;
 
-public class UserRepository {
+import com.zoom.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
