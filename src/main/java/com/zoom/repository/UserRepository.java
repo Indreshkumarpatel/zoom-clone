ba234b05bdb42a8ca0b44c80dc40154dcdf888c8
Imam007 <imimam8409@gmail.com>
2024-09-17 15:26:54 +0530

    update UserRepository

--- src/main/java/com/zoom/repository/UserRepository.java
+++ src/main/java/com/zoom/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package com.zoom.repository;
 
 import com.zoom.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+    User findByEmail(String email);
 }
